d47a0570cacd8163989f4474d32b78e3f8012cd6
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2022-12-18 15:07:21 +0100

    Add project files.

---
+++ EventStreamingPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventStreamingPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
