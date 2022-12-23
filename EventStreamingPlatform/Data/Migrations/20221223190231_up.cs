2f75dc71452cf25f6348d97c0f0b6a2f31f6f884
RinaVeseli <93445391+RinaVeseli@users.noreply.github.com>
2022-12-23 20:18:42 +0100

    d

---
+++ EventStreamingPlatform/Data/Migrations/20221223190231_up.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventStreamingPlatform.Data.Migrations
+{
+    public partial class up : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actors_Movies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors_Movies",
+                columns: table => new
+                {
+                    ActorID = table.Column<int>(type: "int", nullable: false),
+                    MovieID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors_Movies", x => new { x.ActorID, x.MovieID });
+                    table.ForeignKey(
+                        name: "FK_Actors_Movies_Actors_ActorID",
+                        column: x => x.ActorID,
+                        principalTable: "Actors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Actors_Movies_Movies_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actors_Movies_MovieID",
+                table: "Actors_Movies",
+                column: "MovieID");
+        }
+    }
+}
